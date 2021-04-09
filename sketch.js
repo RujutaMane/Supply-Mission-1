fc940c67c74b1edd4d437138824cbc947fc81d6e
RujutaMane <76509583+RujutaMane@users.noreply.github.com>
2021-04-09 03:20:21 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -82,9 +82,20 @@ function draw() {
   }
   
   function keyPressed() {
-   if (keyCode === DOWN_ARROW) {
-	 
-	  packageSprite.velocityY = packageBody.velolcityY
+   if (keyCode === LEFT_ARROW) {
+ 
+ helicopterSprite.x=helicopterSprite.x-20;
+ translation={x:-20,y:0}
+ Matter.Body.translate(packageBody,translation);
+   } else if(keyCode === RIGHT_ARROW) {
+ 
+	helicopterSprite.x=helicopterSprite.x+20;
+	translation={x:20,y:0}
+	Matter.Body.translate(packageBody,translation);
+	  }
+
+	  else if(keyCode === DOWN_ARROW) {
+Matter.Body.setStatic(packageBody,false);
 	  
 	}
   }
